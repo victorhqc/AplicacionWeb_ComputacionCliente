9465a9309cdfc94c25afd30091ee106222de1013
Victor Quiroz Castro <victorhqc@gmail.com>
2019-08-18 20:44:58 +0200

    Remove dead code

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-//
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', `${req.path.slice(1)}.html`));
-// });
-
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
 });
