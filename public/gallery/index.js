09a1f23bf1bd8abe91582e7c7c8f0b3fd277effd
Victor Quiroz Castro <victorhqc@gmail.com>
2019-08-18 20:33:51 +0200

    Add icons for gallery

--- public/gallery/index.js
+++ public/gallery/index.js
@@ -1,4 +1,9 @@
 (function(app) {
+  /**
+   * Constructor de la galería. Inicializa las funciones y valores necesarios.
+   * @param       {string} target Nodo donde se inicializará la galería, ejemplo: '#gallery'
+   * @constructor
+   */
   function Gallery(target) {
     this._setInstance(target);
 
@@ -9,6 +14,10 @@
     this.json = [];
   }
 
+  /**
+   * Inicializa las imagenes y controles de la galería.
+   * @return      {void}
+   */
   Gallery.prototype.init = function init() {
     this._fetchJSON()
       .then(() => {
@@ -17,6 +26,12 @@
       });
   };
 
+  /**
+   * Obtiene el nodo DOM el cual será el nodo padre de la galería. En caso de no encontrarlo,
+   * la función arrojará un error. Además le añade la clase `.gallery`
+   * @param       {string} target
+   * @return      {void}
+   */
   Gallery.prototype._setInstance = function _setInstance(target) {
     this.target = target;
     this.instance = document.querySelector(target);
@@ -30,6 +45,11 @@
     this.instance.className = 'gallery';
   };
 
+  /**
+   * Lee el JSON donde se encuentra la información de las imágenes descargadas por el comando
+   * inicializador. Esto después podría cambiarse a pedir las imágenes directamente a unsplash.com
+   * @return      {Promise}
+   */
   Gallery.prototype._fetchJSON = function _fetchJSON() {
     return new Promise((resolve, reject) => {
       const request = new Request('pictures.json');
@@ -52,6 +72,11 @@
     });
   };
 
+  /**
+   * Elimina los elementos DOM por medio de un query indicado
+   * @param       {string} target Por ejemplo: '.gallery__picture'
+   * @return      {void}
+   */
   Gallery.prototype._clearElements = function _clearElements(target) {
     const pictures = document.querySelectorAll(target);
     pictures.forEach((picture) => {
@@ -59,6 +84,10 @@
     });
   };
 
+  /**
+   * Construye las imágenes en base al JSON
+   * @return {void}
+   */
   Gallery.prototype.buildPictures = function buildPictures() {
     if (!this.json) {
       return;
@@ -77,25 +106,40 @@
     this._setActivePicture();
   };
 
+  /**
+   * Crea y registra los controles de la galería
+   * @return {void}
+   */
   Gallery.prototype.buildControls = function buildControls() {
     this._clearElements('.gallery__control');
 
-    const buildControl = (name) => {
+    const buildControl = (name, iconClassName) => {
       const elm = document.createElement('div');
       elm.classList.add('gallery__control');
       elm.classList.add(`gallery__control--${name}`);
+
+      const icon = document.createElement('span');
+      icon.classList.add('far');
+      icon.classList.add(iconClassName);
+
+      elm.appendChild(icon);
       this.instance.appendChild(elm);
+
 
       return elm;
     };
 
-    const prev = buildControl('previous');
-    const next = buildControl('next');
+    const prev = buildControl('previous', 'fa-angle-right');
+    const next = buildControl('next', 'fa-angle-left');
 
     prev.addEventListener('click', this.previous, false);
     next.addEventListener('click', this.next, false);
   };
 
+  /**
+   * Obtiene la siguiente imagen.
+   * @return {void}
+   */
   Gallery.prototype.next = function next() {
     if (this.activePicture === this.json.length - 1) {
       this.activePicture = 0;
@@ -106,6 +150,10 @@
     this._setActivePicture();
   };
 
+  /**
+   * Obtiene la imagen anterior.
+   * @return {void}
+   */
   Gallery.prototype.previous = function previous() {
     if (this.activePicture === 0) {
       this.activePicture = this.json.length - 1;
@@ -116,6 +164,11 @@
     this._setActivePicture();
   };
 
+  /**
+   * Le asigna la clase indicada a la imágen activa. Esto es necesario para que se aplique la
+   * animación de CSS
+   * @return {void}
+   */
   Gallery.prototype._setActivePicture = function _setActivePicture() {
     const currentActivePicture = document.querySelector('.gallery__picture--active');
     if (currentActivePicture) {
@@ -124,7 +177,23 @@
 
     const picture = document.querySelectorAll('.gallery__picture')[this.activePicture];
     picture.classList.add('gallery__picture--active');
+
+    this._updateControlsColor();
   };
 
+  Gallery.prototype._updateControlsColor = function _updateControlsColor() {
+    const picture = this.json[this.activePicture];
+    if (!picture) return;
+
+    const controls = document.querySelectorAll('.gallery__control .far');
+    controls.forEach((control) => {
+      control.style.color = picture.color;
+    });
+  };
+
+  /**
+   * Registra la clase Gallery en `app` (en este caso es window). Esto es necesario para poder
+   * utilizar la galería en otro `<script />`
+   */
   app.Gallery = Gallery;
 })(window);
